1d1c3fa4792f023cc73aaa315392edac156ec17a
muhammadnurulahsan <mdnurulahsanbdbd@gamil.com>
2022-11-21 14:59:42 +0600

    figma file added

--- .storybook/decorators.tsx
+++ .storybook/decorators.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { DecoratorFn } from "@storybook/react";
 import { GlobalStyle } from "../src/styles/GlobalStyle";
+import { withDesign } from "storybook-addon-designs";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../src/styles/theme";
 
 const withTheme: DecoratorFn = (StoryFn, context) => {
-  const theme = context.globals.theme;
+  const theme = context.parameters.theme || context.globals.theme;
   const storyTheme = theme === "dark" ? darkTheme : lightTheme;
 
   return (
@@ -16,4 +17,4 @@ const withTheme: DecoratorFn = (StoryFn, context) => {
   );
 };
 
-export const globalDecorators = [withTheme];
+export const globalDecorators = [withTheme, withDesign];
--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,15 +1,25 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import { Badge } from './Badge'
+import { Badge } from "./Badge";
 
 export default {
-  title: 'Components/Badge',
+  title: "Components/Badge",
   component: Badge,
   args: {
-    text: 'Comfort Food',
+    text: "Comfort Food",
   },
-} as ComponentMeta<typeof Badge>
+  parameters: {
+    design: {
+      type: "figspec",
+      url: "https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1906%3A3469",
+    },
+  },
+} as ComponentMeta<typeof Badge>;
 
-const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
 
-export const Default = Template.bind({})
+export const Default = Template.bind({});
+export const DarkTheme = Template.bind({});
+DarkTheme.parameters = {
+  theme: "dark",
+};
